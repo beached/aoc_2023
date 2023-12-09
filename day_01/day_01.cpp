e94cc9effac739e5b526617a2a6cf5fbf2b45452
Darrell Wright <beached@users.noreply.github.com>
2023-12-08 23:54:17 -0500

    Added day 1 stub

---
+++ day_01/day_01.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Darrell Wright
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/beached/aoc_2023
+//
+
+
+
+int main( ) {
+
+}
